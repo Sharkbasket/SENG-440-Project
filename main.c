22b01c606b799fda1983dbc1fa05d17cd2711abc
Shaun McGuigan <Shaun@Goldenrod.local>
2020-06-21 17:19:23 -0700

    Print out the bytes in prettier format

--- main.c
+++ main.c
@@ -5,22 +5,29 @@
 #define FILE_NAME "1024x768-Jerry-TestPattern.png"
 #define WIDTH 1024
 #define HEIGHT 768
-#define CHANNELS 4
+#define CHANNELS 4 // The source image file has an additional alpha channel
 
 int main()
 {
     int x = WIDTH;
     int y = HEIGHT;
     int n = CHANNELS;
+    // Ignore the alpha channel and just return RGB pixel data
     unsigned char* pixel_data = stbi_load(FILE_NAME, &x, &y, &n, STBI_rgb);
     
-    for (int i = 0; i < 1024; i++)
+    // This is just a test to see some of the pixel data printed out
+    for (int i = 0; i < 1200; i++)
     {
         if ((i % 3) == 0)
         {
-            printf("\n");
+            printf(" ");
+            
+            if ((i % 24) == 0)
+            {
+                printf("\n");
+            }
         }
-        printf("%2x ", *(pixel_data++));
+        printf("%02X", *(pixel_data++));
     }
     
     return 0;
